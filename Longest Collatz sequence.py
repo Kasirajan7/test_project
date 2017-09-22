174d3fc628c3bbfdfb6a8d4cd2f31047b9909686
Kasirajan <kasimurugasen1995@gmail.com>
2017-09-22 13:32:54 +0530

    Longest Collatz sequence
    modified using if case  but still shooting timeout problem

--- Longest Collatz sequence.py
+++ Longest Collatz sequence.py
@@ -1,27 +1,52 @@
 n=int(input())
 c=[int(input()) for i in range(n)]
+max=0
 for i in range(n):
     a=c[i]
-    max=0
+   
+    if i>0 and c[i]>c[i-1]:
+        for i in range(c[i-1],c[i]+1):
+            j=i
+            #print(j)
+            count=0
+            while j:
+                if j%2==0:
+                    while j%2==0:
+                        j=j//2
+                        count+=1
+                else:
+                    while j%2!=0:
+                        j=(3*j)+1
+                        count+=1
+                if j==1:
+                    break
+            #print(i,count)
+            if count>=max:
+                #print(count,i)
+                max=count
+                k=i
+        print(k)
+    else:
+        max=0
+        for i in range(1,c[i]+1):
+            j=i
+            #print(j)
+            count=0
+            while j:
+                if j%2==0:
+                    while j%2==0:
+                        j=j//2
+                        count+=1
+                else:
+                    while j%2!=0:
+                        j=(3*j)+1
+                        count+=1
+                if j==1:
+                    break
+            #print(i,count)
+            if count>=max:
+                #print(count,i)
+                max=count
+                k=i
+        print(k)
     
-    for i in range(1,c[i]+1):
-        j=i
-        #print(j)
-        count=0
-        while j:
-            if j%2==0:
-                while j%2==0:
-                    j=j//2
-                    count+=1
-            else:
-                while j%2!=0:
-                    j=(3*j)+1
-                    count+=1
-            if j==1:
-                break
-        #print(i,count)
-        if count>=max:
-            #print(count,i)
-            max=count
-            k=i
-    print(k)
