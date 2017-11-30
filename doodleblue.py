6b7e63b854c725119c0c707edc3bd7ed5bcad8da
Kasirajan <kasimurugasen1995@gmail.com>
2017-11-30 13:25:21 +0530

    Scheduling dates and displaying in csv file
    Task:
    // input -> ["monday", "tuesday"] or ["tuesday", "wednesday", "wednesday"] 
    // inputs can be wary
    
    // input type -> json data
    
    1: using flask Create a scheduling system for 28 days from the current day 
    
    explanation -> if your inputs are "Monday" and "Wednesday" then create records in the database for each week one for Monday and another for Wednesday from the current day, continue this until the 28 days. and display the final result.
    
    
    database - any
    
    paradigm - oops
    
    id  ||  day       || schedule || current_datetime
    
    -------------------------------------------
    
    1   ||  monday    || 1        || 2017-11-28 11:52:39.480488
    
    2   ||  wednesday || 2        || 2017-11-28 11:52:39.480497
    
    ..  ||  ..        || ..       || ..

--- doodleblue.py
+++ doodleblue.py
@@ -2,53 +2,62 @@ import datetime
 import io
 import json
 import sys
+import calendar
+
 class Doodle:
 
-    def __init__(self):
-        self.schedule=1
-        self.id1=1
+    def __init__(self):#constructor class
+        self.schedule=1#initialize schedule
+        self.id1=1#initialize id
 
-    def getjson(self):
+    def getjson(self):#get string and converts into json_array
         self.string=input("Enter the data in json format: ")#gets string
         self.json_array=json.loads(self.string)#converts into json array using load function
 
-    def check_days(self):
-        for i in self.json_array:
-            i=i.strip()
-            if i.lower()=="monday" or i.lower()=="tuesday" or i.lower()=="wednesday" or i.lower()=="thursday" or i.lower()=="friday" or i.lower()=="saturday" or i.lower()=="sunday":
+    def check_days(self):#check days
+        self.weekdays=["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+        self.input=[]
+        for i in self.json_array:#check whether the days are valid days if "fridis" is given it returns False else True
+            i=i.strip()   #it removes spaces in front and back of the string
+            
+            if i.lower() in self.weekdays:
+                if i.lower() in self.input:# if ["monday","wednesday","wednesday"] is given It changes to ["monday","wednesday"]
+                    pass
+                else:
+                    self.input.append(i.lower())
                 count=True
-            else:
+            else :
                 count=False
-                break
+                break#loops breaks when there is invalid data
         return count
-    def date(self):
-        self.now=datetime.datetime.now()
-        self.current_datetime=self.now.strftime("%Y-%m-%d %H:%M")
+    
+    
 
-    def writefile(self):
-        
-        filename="schedule.csv"
-        f=io.open(filename,"w",encoding="utf-8")
-        headers="ID , Day , Schedule , Current_datetime \n"
-        f.write(headers)
+    def writefile(self):#writing output to the csv file which shows the output in the table format
+        now=datetime.date.today()#it gets the current date
+        filename="schedule.csv"# csv file name it will be created in the working directory
+        f=io.open(filename,"w",encoding="utf-8")#opening a csv five in write mode
+        headers="ID, Day, Schedule, Current_datetime \n"
+        f.write(headers)#writing the column headers into csv file
+        for i in range(1,29):
+            day=calendar.day_name[now.weekday()]#it returns weekdays [ie:Monday,Tuesday,Wednesday,etc] of the date 
+            if day.lower() in self.input:#it checks whether the current day has to be schedule or not
+                current_datetime=(str(now).replace("-",".")+"."+str(datetime.datetime.now().time()))#it concatenates date and time
+                
+                f.write(str(self.id1)+"\t" + "," + str(day.upper())+"\t"  + "," + str(self.schedule)+"\t"  + "," + current_datetime +"\n")#it writes id , weekday,schedule,datetime
+                self.id1+=1
+                self.schedule+=1
+            now= datetime.date.today()+datetime.timedelta(days=i)#it increments next day
+        f.close()# file closed
 
-        for i in range(28):
-            day=self.json_array[self.schedule%len(self.json_array)].strip()
-            f.write(str(self.id1) + "," + str(day.upper()) + "," + str(self.schedule) + "," + self.current_datetime.replace(" ",".") +"\n")
-            self.id1+=1
-            self.schedule+=1
-
-        f.close()
-
-doodle=Doodle()
+doodle=Doodle()#creating object for class
 doodle.getjson()
 n=doodle.check_days()
-if n==True:
-    doodle.date()
+if n==True:#if valid days is given it writes into the file else quit
     doodle.writefile()
     print("Your days Scheduled successfully")
     print("Open your working directory and open Schedule.csv file")
-    print("Note :Click Yes ")
+    print("Note :Click Yes and OK while opening csv file ")#caution
 else:
     print("Inalid days")
     quit()
